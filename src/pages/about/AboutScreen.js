398b08f6b2385ab0216ce5f39f2de2aaeaa060fd
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2022-06-08 03:03:18 +0630

    Update About Me

--- src/pages/about/AboutScreen.js
+++ src/pages/about/AboutScreen.js
@@ -7,7 +7,7 @@ function AboutScreen() {
       <div className="p-4">
         <div className="bg-white rounded-xl p-3 font-title">
           <PageTitle title="About Me" />
-          <div className="my-5 flex flex-row items-center">
+          <div className="mt-4 mb-5 flex flex-row items-center">
             <img
               src="https://raw.githubusercontent.com/lwinmoepaing/lwinmoepaing/main/img/gitto.gif"
               className="h-12 mr-3"
@@ -22,7 +22,7 @@ function AboutScreen() {
               >
                 Hi, I'm Lwin Moe Paing
               </a>
-              <h1> This is Free Simple-Invoice Maker for your business.</h1>
+              <h1> Free Invoice Maker for your business.</h1>
             </div>
           </div>
 
@@ -35,7 +35,7 @@ function AboutScreen() {
               <li> Can Export Image </li>
             </ul>
           </div>
-          <div className="font-title mt-4">
+          <div className="font-title mt-3 mb-5">
             <div>
               🤝 I’m looking for help with JS dev to build Many Free Softwares
               to help people who can't afford !!
@@ -59,16 +59,40 @@ function AboutScreen() {
               </a>
             </div>
             <div>
-            🎁 {" "}
+              <span>🎁 </span>
               <a
                 href="https://github.com/lwinmoepaing/invoice-maker"
                 className="underline cursor-pointer"
                 target={"_blank"}
                 rel="noreferrer"
               >
+                {" "}
                 Repo Link Here
               </a>
             </div>
+          </div>
+
+          <PageTitle title="Build By" />
+          <div className="mt-2 mb-5 pl-4 text-sm">
+            <ul class="list-disc">
+              <li> Framer Motion For each component Animation</li>
+              <li> Lottiefiles For Dashboard Widgets Icons</li>
+              <li> IndexedDB for Local Storage </li>
+              <li> ReactJS </li>
+            </ul>
+          </div>
+
+          <PageTitle title="Contact" />
+          <div className="mt-2 pl-1 text-sm">
+            <a
+              href="tel:+959420059241"
+              className="underline cursor-pointer"
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              {" "}
+              +959420059241
+            </a>
           </div>
         </div>
       </div>
